510955e9eb53585b0d3101b64e3195c3c13eb7d3
Mike Archer <mlarcher531@gmail.com>
2020-07-30 15:39:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,7 +10,7 @@ end
 
 def monopoly_with_third_tier
   new_hash = {
-    railroads: {pieces: 4, rent_in_dollars: {one_piece_owned: 25}, names: {}
+    railroads: {pieces: 4, rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50}, names: {}
   }
   }
 
