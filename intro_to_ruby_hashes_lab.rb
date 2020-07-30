2283fc9837a1802c044039d196cd9b40d0b10d77
Mike Archer <mlarcher531@gmail.com>
2020-07-30 15:36:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -4,7 +4,7 @@ new_hash = {railroads: {}}
 end
 
 def monopoly_with_second_tier
-  new_hash = {railroads: {pieces: 4, rent_in_dollars: {}}}
+  new_hash = {railroads: {pieces: 4, rent_in_dollars: {}, names: {}}}
 
 end
 
